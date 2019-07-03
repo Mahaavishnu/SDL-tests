c2b821653f84ebc545f30ef7bef3dcbe5cacd089
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-03 02:08:41 +0200

    Dégradé de surfaces

--- test.c
+++ test.c
@@ -7,8 +7,9 @@ int			main(int argc, char *argv[])
 	SDL_Window *win;
 	SDL_Renderer *ren;
 	SDL_Surface *screen;
-	SDL_Surface *drawing;
+	SDL_Surface *drawing[256][700];
 	SDL_Rect position;
+	int x, y;
 
 	if (SDL_Init(SDL_INIT_VIDEO) == -1)
 	{
@@ -16,7 +17,7 @@ int			main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	win = SDL_CreateWindow("Hellow World", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow("Hellow World", 100, 100, 700, 256, SDL_WINDOW_SHOWN);
 	if (win == NULL)
 	{
 		fprintf(stderr, "SDL_CreateWindow Error : %s\n", SDL_GetError());
@@ -24,25 +25,37 @@ int			main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	/*ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == NULL)
 	{
 		fprintf(stderr, "SDL_CreateRenderer Error : %s\n", SDL_GetError());
 		SDL_DestroyWindow(win);
 		SDL_Quit();
 		exit(EXIT_FAILURE);
-	}
+	}*/
 
 
 	screen = SDL_GetWindowSurface(win);
-	SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 250, 0, 0));
+	//SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 250, 0, 0));
 
-	position.x = 50;
-	position.y = 50;
+	for (x = 0; x < 256; x++)
+	{
+		for (y = 0; y < 700; y++)
+		{
+			position.x = y;
+			position.y = x;
+			drawing[x][y] = SDL_CreateRGBSurface(SDL_SWSURFACE, 1, 1, 32, 0, 0, 0, 0);
+			SDL_FillRect(drawing[x][y], NULL, SDL_MapRGB(screen->format, x, x, x));
+			SDL_BlitSurface(drawing[x][y], NULL, screen, &position);
+		}
+		y = 0;
+	}
+	//position.x = 50;
+	//position.y = 50;
 
-	drawing = SDL_CreateRGBSurface(SDL_SWSURFACE, 100, 100, 32, 0, 0, 0, 0);
-	SDL_FillRect(drawing, NULL, SDL_MapRGB(screen->format, 0, 255, 0));
-	SDL_BlitSurface(drawing, NULL, screen, &position);
+	//drawing = SDL_CreateRGBSurface(SDL_SWSURFACE, 100, 100, 32, 0, 0, 0, 0);
+	//SDL_FillRect(drawing, NULL, SDL_MapRGB(screen->format, 0, 255, 0));
+	//SDL_BlitSurface(drawing, NULL, screen, &position);
 
 	if (SDL_UpdateWindowSurface(win) != 0)
 	{
